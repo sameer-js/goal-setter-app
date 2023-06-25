2be8994cbe03a21d78ac507a84c6bbffce2e1327
Sameer Jung Subedi <meersj@gmail.com>
2023-06-25 17:16:07 +0545

    updated backend endpoint

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/users/';
+const API_URL = 'https://goal-setter-app-ch31.onrender.com/api/users/';
 
 // Register User
 const register = async (userData) => {
--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '/api/goals/';
+const API_URL = 'https://goal-setter-app-ch31.onrender.com/api/goals/';
 
 // Create new goal
 const createGoal = async (goalData, token) => {
