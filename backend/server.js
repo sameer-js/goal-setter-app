cdd9c7831c72e6beff500739e812243891e05f8a
Sameer Jung Subedi <meersj@gmail.com>
2023-06-25 17:45:12 +0545

    updated for CORS

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.port || 8000;
@@ -13,6 +14,11 @@ const app = express();
 // need these middlewares for POST and PUT requests as they SEND data which is enclosed in the body (req.body)
 app.use(express.json()); // recognize incoming request as json object
 app.use(express.urlencoded({ extended: false })); // parses the incoming request with urlencoded payloads and is based upon the body-parser
+app.use(cors());
+app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.send('API is running');
+});
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
